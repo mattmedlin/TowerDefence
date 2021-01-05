2cded0895d23d74929227442f35f91b5c616b9ed
mattmedlin <matthewkmedlin94@gmail.com>
2021-01-05 12:38:05 -0600

    waypoints

--- Tower Defence/Assets/Scripts/EnemyMovement.cs
+++ Tower Defence/Assets/Scripts/EnemyMovement.cs
@@ -4,24 +4,22 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] List<Block> path;
+    [SerializeField] List<Waypoint> path;
 
     // Start is called before the first frame update
     void Start()
     {
-        PrintAllWaypoints();
+        StartCoroutine(FollowPath());
     }
-    private void PrintAllWaypoints()
+    IEnumerator FollowPath()
     {
-        foreach (Block waypoint in path)
+        print("Starting patrol...");
+        foreach (Waypoint waypoint in path)
         {
-            print(waypoint.name);
+            transform.position = waypoint.transform.position;
+            print("Visiting: " + waypoint);
+            yield return new WaitForSeconds(1f);
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        print("Ending patrol");
     }
 }
