84066682fd8dce5c27a6e60d68ac61353d7c79d6
mattmedlin <matthewkmedlin94@gmail.com>
2021-01-05 15:14:36 -0600

    waypoint and refactoring

--- Tower Defence/Assets/Scripts/CubeEditor.cs
+++ Tower Defence/Assets/Scripts/CubeEditor.cs
@@ -4,12 +4,15 @@ using UnityEngine;
 
 [ExecuteInEditMode]
 [SelectionBase]
+[RequireComponent(typeof(Waypoint))]
 public class CubeEditor : MonoBehaviour
 {
-    const float gridSize = 10f;
+    Waypoint waypoint;
 
-    TextMesh textMesh;
-    Vector3 gridPos;
+    private void Awake() 
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
 
     void Start()
     {
@@ -18,15 +21,26 @@ public class CubeEditor : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        SnapToGrid();
+        UpdateLabel();
+    }
 
-        gridPos.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
-        gridPos.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
-        transform.position = new Vector3(gridPos.x, 0f, gridPos.z);
+    private void SnapToGrid()
+    {
+        int gridSize = waypoint.GetGridSize();
 
-        textMesh = GetComponentInChildren<TextMesh>();
-        string labelText = gridPos.x / gridSize + "," + gridPos.z / gridSize;
+        transform.position = new Vector3(
+            waypoint.GetGridPos().x, 
+            0f, 
+            waypoint.GetGridPos().y);
+    }
+
+    private void UpdateLabel()
+    {
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        int gridSize = waypoint.GetGridSize();
+        string labelText = waypoint.GetGridPos().x / gridSize + "," + waypoint.GetGridPos().y / gridSize;
         textMesh.text = labelText;
         gameObject.name = "Cube " + labelText;
-
     }
 }
