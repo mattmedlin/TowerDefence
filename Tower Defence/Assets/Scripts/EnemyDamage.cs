239c2fd39ce40cb2b4b9e682acc4ba7c1c043f8d
mattmedlin <matthewkmedlin94@gmail.com>
2021-01-22 12:55:50 -0600

    added particle systems
    enemy hit particles and death particles

--- Tower Defence/Assets/Scripts/EnemyDamage.cs
+++ Tower Defence/Assets/Scripts/EnemyDamage.cs
@@ -6,6 +6,8 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     [SerializeField] int hitPoints = 10;
+    [SerializeField] ParticleSystem hitParticlePrefab;
+    [SerializeField] ParticleSystem deathParticlePrefab;
 
     // Start is called before the first frame update
     void Start()
@@ -15,15 +17,21 @@ public class EnemyDamage : MonoBehaviour
 
     private void OnParticleCollision(GameObject other)
     {
+        Debug.Log(gameObject.name + " hit by " + other.name);
+
         ProcessHit();
         if (hitPoints <= 0)
         {
+            var vfx = Instantiate(deathParticlePrefab, transform.position, Quaternion.identity);
+            vfx.Play();
             Destroy(gameObject);
         }
     }
 
     void ProcessHit()
     {
+        hitParticlePrefab.Play();
         hitPoints -= 1;
+
     }
 }
